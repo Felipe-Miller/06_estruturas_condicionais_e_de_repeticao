7e7719ebcafbd439d0f99cea729b47c2693a7985
Felipe Miller <fmvargas@protonmail.com>
2023-08-15 13:50:43 -0300

    exercício 8

---
+++ exercicio_VIII.js
@@ -0,0 +1,8 @@
+let salario = parseFloat(prompt("Digite o salário do usuário:"));
+
+if (salario < 1903.98) {
+    document.write("ISENTO DE IR");
+} else {
+    document.write("TRIBUTADO NO IR");
+}
+
