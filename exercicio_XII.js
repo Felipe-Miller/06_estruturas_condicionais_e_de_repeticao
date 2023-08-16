ca2786d8e812e076d8a73cdfa560a2abf384726f
Felipe Miller <fmvargas@protonmail.com>
2023-08-15 22:42:01 -0300

    exercício 12

---
+++ exercicio_XII.js
@@ -0,0 +1,7 @@
+const idade = parseInt(prompt("Qual é a sua idade?"));
+
+if (idade >= 18 && idade <= 67) {
+    alert("Você pode doar sangue!");
+} else {
+    alert("Você não pode doar sangue.");
+}
